0b53d8a590652393cd01f888e89f04bdaa76bfff
juliusheese <julius.42.shapiro@gmail.com>
2020-08-09 19:39:03 -0500

    2/10

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.querySelector("header").addEventListener("mouseover", function() {
+    document.querySelector("p").style.color = "green";
+});
+document.addEventListener("keydown", function() {
+    console.log('keydown');
+    document.querySelector(".content-destination p").style.color = "red";
+});
